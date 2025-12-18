d79bb4bdffddecdfb1ef233dbbb81fb8ce0136a5
JiraKorn404 <korn7716@gmail.com>
2025-12-18 11:47:32 +0700

    add create_db_and_table function

--- app/app.py
+++ app/app.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from app.schemas import PostCreate, PostResponse
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
 
 app = FastAPI()
 
@@ -10,23 +18,3 @@ text_posts = {1: {'title': 'New Post', 'content': 'Cool test post'},
               5: {'title': 'Update', 'content': 'Just launched my new project! Excited to share more soon.'},
               6: {'title': 'Tech Insight', 'content': 'Async IO in Python can massively speed up I/O-bound tasks.'},
               7: {'title': 'Quote', 'content': "'Programs must be written for people to read, and only incidentally for mach"}}
-
-@app.get('/posts')
-def get_all_post(limit: int = None):
-    if limit:
-        return list(text_posts.values())[:limit]
-    return text_posts
-
-@app.get('/posts/{id}')
-def get_post(id: int) -> PostResponse:
-    if id not in text_posts:
-        raise HTTPException(status_code=404, detail='Post not found')
-
-    return text_posts.get(id)
-
-@app.post('/posts')
-def create_post(post: PostCreate) -> PostResponse:
-    new_post = {'title': post.title, 'content': post.content}
-    text_posts[max(text_posts.keys()) + 1 ] = new_post
-    return new_post
-
